9da569099c6289416259b4fb5755377736271904
Pritam Das <pritamd47@gmail.com>
2026-03-14 22:18:31 +0530

    update packaging info

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "rat",
-    version = "develop",
+    version = "3.0.19",
     license = "GPL-3.0",
     package_dir = {"": "src"}
 )
